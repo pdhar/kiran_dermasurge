eed3fbff690c91259c0a8c92679a7ccd4a35443b
pdhar <pranav.dhar2@gmail.com>
2013-05-07 15:07:16 +0530

    action mailer mailto updates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
   def contact_us(query)
     @query = query
     
-    mail to: "info@kirandermasurge.com", subject: @query.subject
-    mail to: "pranav.dhar2@gmail.com", subject: @query.subject
+    #mail to: "info@kirandermasurge.com", subject: @query.subject
+    mail to: "pranav.dhar2@gmail.com", subject: @query.subject, cc: "info@kirandermasurge.com" , bcc: "pdhar@kirandermasurge.com"
   end
 end
